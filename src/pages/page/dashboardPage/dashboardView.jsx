4e7c625a4d5918ba3d7afecc5fe0705da5a05f79
howhigare <aylasyafaa23@gmail.com>
2024-06-26 20:26:41 +0700

    feat:[AS] buat chart di dashboard

--- src/pages/page/dashboardPage/dashboardView.jsx
+++ src/pages/page/dashboardPage/dashboardView.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { LineChart } from '@mui/x-charts/LineChart';
 import Sidebar from "../../../components/sidebar";
 
-const dashboardPage = () => {
+const DashboardPage = () => {
   return (
     <div className="container-fluid flex">
       <Sidebar />
       <div className="flex-1 flex flex-col p-10 ml-20 sm:ml-64">
-        <div className=" text-black">huhuh</div>
+        <div className="text-black">istri jaehyun</div>
+        <div>
+          <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+              {
+                data: [2, 5.5, 2, 8.5, 1.5, 5],
+              },
+            ]}
+            height={500}
+            margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+            grid={{ vertical: true, horizontal: true }}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default dashboardPage;
+export default DashboardPage;
