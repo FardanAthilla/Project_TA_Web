6d8c4273061caec61892ee9ee3c79cc6d9601b6c
howhigare <aylasyafaa23@gmail.com>
2024-07-08 23:23:15 +0700

    feat[AS] hit 2 API buat charts diagram penjualan dan service nya

--- src/pages/page/dashboardPage/dashboardView.jsx
+++ src/pages/page/dashboardPage/dashboardView.jsx
@@ -56,7 +56,7 @@ const DashboardPage = () => {
     <div className="container-fluid flex">
       <Sidebar />
       <div className="flex-1 flex flex-col p-10 ml-20 sm:ml-64">
-        <div className="text-black">Dashboard UD Mojopahit!!</div>
+        <div className="text-black">Dashboard UD Mojopahit!!!</div>
         <div>
           <LineChart
             xAxis={[{ data: dates }]}
