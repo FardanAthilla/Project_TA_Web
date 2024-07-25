988daf49be9d951599872c087923248f44436ba2
howhigare <aylasyafaa23@gmail.com>
2024-07-25 07:19:13 +0700

    feat:[AS] make table and hit the api

--- src/pages/page/historyanalytics/service.jsx
+++ src/pages/page/historyanalytics/service.jsx
@@ -1,18 +1,157 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Sidebar from '../../../components/sidebar';
+import { format } from 'date-fns';
+import { id } from 'date-fns/locale';
+import DataTable from 'react-data-table-component';
 
-function ServiceView() {
+const ServiceView = () => {
+  const [serviceData, setServiceData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://rdo-app-o955y.ondigitalocean.app/service');
+        const result = response.data;
+
+        const formattedServiceData = result.Data.map((service) => ({
+          id: service.service_report_id,
+          date: format(new Date(service.date), 'eeee, dd MMMM yyyy', { locale: id }),
+          person_name: service.person_name,
+          machine_number: service.machine_number,
+          machine_name: service.machine_name,
+          complaints: service.complaints,
+          status: service.status,
+        }));
+
+        setServiceData(formattedServiceData);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const serviceColumns = [
+    {
+      name: 'ID',
+      selector: row => row.id,
+      sortable: true,
+      width: '10%',
+    },
+    {
+      name: 'Tanggal',
+      selector: row => row.date,
+      sortable: true,
+      width: '25%',
+    },
+    {
+      name: 'Nama Personil',
+      selector: row => row.person_name,
+      sortable: true,
+      width: '20%',
+    },
+    {
+      name: 'Nomor Mesin',
+      selector: row => row.machine_number,
+      sortable: true,
+      width: '15%',
+    },
+    {
+      name: 'Nama Mesin',
+      selector: row => row.machine_name,
+      sortable: true,
+      width: '15%',
+    },
+    {
+      name: 'Keluhan',
+      selector: row => row.complaints,
+      sortable: true,
+      width: '25%',
+    },
+    {
+      name: 'Status',
+      selector: row => row.status,
+      sortable: true,
+      width: '15%',
+    },
+  ];
+
+  const customStyles = {
+    headCells: {
+      style: {
+        backgroundColor: '#cfe2ff',
+        fontSize: '16px',
+        fontWeight: 'bold',
+      },
+    },
+  };
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = serviceData.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(serviceData.length / itemsPerPage);
+
   return (
-    <div className="flex">
+    <div className="h-screen flex bg-white">
       <Sidebar />
-      <div className="flex-grow p-4">
-        <h1>Analytics Service Page</h1>
-        {
-            
-        }
+      <div className="flex-1 flex flex-col p-10 ml-20 sm:ml-64">
+        {loading ? (
+          <div className="text-center text-2xl">Sebentar, sedang memuat</div>
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold mb-4">Analitik service</h1>
+            <DataTable
+              columns={serviceColumns}
+              data={currentItems}
+              customStyles={customStyles}
+              pagination={false}
+            />
+            <div className="flex justify-center mt-4">
+              <div className="join pt-5">
+                <button
+                  className="join-item btn"
+                  onClick={() => paginate(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  «
+                </button>
+                {Array.from({ length: totalPages }).map((_, index) => {
+                  const pageNumber = index + 1;
+                  return (
+                    <button
+                      key={index}
+                      onClick={() => paginate(pageNumber)}
+                      className={`join-item btn ${pageNumber === currentPage ? "active" : ""}`}
+                    >
+                      {pageNumber}
+                    </button>
+                  );
+                })}
+                <button
+                  className="join-item btn"
+                  onClick={() => paginate(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                >
+                  »
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default ServiceView;
