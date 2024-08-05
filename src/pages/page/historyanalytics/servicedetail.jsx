2cc0f26d2c77176c38b96c27a738898a7b93e3c9
howhigare <aylasyafaa23@gmail.com>
2024-08-05 12:30:40 +0700

    feat:[AS] membenarkan design detail

--- src/pages/page/historyanalytics/servicedetail.jsx
+++ src/pages/page/historyanalytics/servicedetail.jsx
@@ -8,7 +8,7 @@ const ServiceDetail = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen flex bg-gray-100">
+    <div className="h-screen flex bg-white">
       <Sidebar />
       <main className="flex-1 flex flex-col p-10 ml-20 sm:ml-64">
         <header className="flex justify-between items-center mb-4">
@@ -22,44 +22,112 @@ const ServiceDetail = () => {
         </header>
 
         <section className="bg-white p-6 rounded-lg shadow-md">
-          <div className="flex mb-6">
-            {/* <img
+          <div className="flex mb-4 items-center">
+            <img
               src={service.worker_image}
               alt="Worker"
               className="w-20 h-20 rounded-full mr-4 object-cover"
-            /> */}
+            />
             <div>
               <h2 className="text-xl font-bold text-black">{service.worker_name}</h2>
-              <p className="text-gray-600">{service.date}</p>
+              <p className="text-lg text-gray-600">{service.date}</p>
             </div>
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div className="detail-item">
-              <h3 className="text-lg font-semibold text-gray-700">
-                Nama Pelanggan:
-              </h3>
-              <p>{service.customer_name}</p>
+            <div className="sm:col-span-4 mb-4">
+              <label
+                htmlFor="customer_name"
+                className="block text-md font-medium leading-6"
+              >
+                Nama Pelanggan
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="customer_name"
+                  id="customer_name"
+                  value={service.customer_name}
+                  readOnly
+                  className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
             </div>
-            <div className="detail-item">
-              <h3 className="text-lg font-semibold text-gray-700">
-                Nama Mesin:
-              </h3>
-              <p>{service.machine_name}</p>
+
+            <div className="sm:col-span-4 mb-4">
+              <label
+                htmlFor="machine_name"
+                className="block text-md font-medium leading-6"
+              >
+                Nama Mesin
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="machine_name"
+                  id="machine_name"
+                  value={service.machine_name}
+                  readOnly
+                  className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
             </div>
-            <div className="detail-item">
-              <h3 className="text-lg font-semibold text-gray-700">
-                Nomor Mesin:
-              </h3>
-              <p>{service.machine_number}</p>
+
+            <div className="sm:col-span-4 mb-4">
+              <label
+                htmlFor="machine_number"
+                className="block text-md font-medium leading-6"
+              >
+                Nomor Mesin
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="machine_number"
+                  id="machine_number"
+                  value={service.machine_number}
+                  readOnly
+                  className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
             </div>
-            <div className="detail-item">
-              <h3 className="text-lg font-semibold text-gray-700">Keluhan:</h3>
-              <p className="text-wrap">{service.complaints}</p>
+
+            <div className="sm:col-span-4 mb-4">
+              <label
+                htmlFor="complaints"
+                className="block text-md font-medium leading-6"
+              >
+                Keluhan
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="complaints"
+                  id="complaints"
+                  value={service.complaints}
+                  readOnly
+                  className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
             </div>
-            <div className="detail-item">
-              <h3 className="text-lg font-semibold text-gray-700">Status:</h3>
-              <p>{service.status}</p>
+
+            <div className="sm:col-span-4 mb-4">
+              <label
+                htmlFor="status"
+                className="block text-md font-medium leading-6"
+              >
+                Status
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="status"
+                  id="status"
+                  value={service.status}
+                  readOnly
+                  className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md sm:leading-6"
+                />
+              </div>
             </div>
           </div>
         </section>
